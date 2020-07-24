968f765b99ea58b0268dae4c2f4a10d07f3c6cad
sherry <sherry.bajwa@idc.bedbath.com>
2020-07-24 17:56:54 +0530

    Added Dependency injections(Coordinators) To the structure To application layer and presentation layer

---
+++ ReactSwiftMVVMAssignment/ReactSwiftMVVMAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReactSwiftMVVMAssignment
+//
+//  Created by Sherry Bajwa on 24/07/20.
+//  Copyright © 2020 Sherry Bajwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
